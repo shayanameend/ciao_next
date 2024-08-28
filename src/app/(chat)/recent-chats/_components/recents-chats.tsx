a5b274bbed7604604ef9a9875d3982c175dce1f7
Shayan Ameen <shayan.ameen.developer@gmail.com>
2024-08-29 02:46:39 +0500

    message trailing dots if gretear than specified limit

--- src/app/(chat)/recent-chats/_components/recents-chats.tsx
+++ src/app/(chat)/recent-chats/_components/recents-chats.tsx
@@ -33,59 +33,58 @@ export function RecentsChats({
 				<CardTitle>Recent Chats</CardTitle>
 			</CardHeader>
 			<CardContent className="grid gap-6">
-				{privateChats.map((chat) => (
-					<div
-						key={chat.id}
-						className="flex items-center justify-between space-x-4"
-					>
-						<div className="flex items-center space-x-4">
-							<Avatar className="h-8 w-8">
-								<AvatarFallback>
-									{
-										chat.members.find(
-											(member) => member.id !== onlineUsers[0]?.id,
-										)?.fullName?.[0]
-									}
-								</AvatarFallback>
-							</Avatar>
-							<div>
-								<p className="text-sm font-medium leading-none">
-									{
-										chat.members.find(
-											(member) => member.id !== onlineUsers[0]?.id,
-										)?.fullName
-									}
-								</p>
-								<p className="text-sm text-muted-foreground">
-									{chat.messages[0].text}
-								</p>
+				{privateChats.map((chat) => {
+					const fullName = chat.members.find(
+						(member) => member.id !== onlineUsers[0]?.id,
+					)?.fullName;
+
+					const lastMessage =
+						chat.messages[0]?.text?.length > 20
+							? `${chat.messages[0]?.text.slice(0, 20)}...`
+							: chat.messages[0]?.text;
+
+					return (
+						<div
+							key={chat.id}
+							className="flex items-center justify-between space-x-4"
+						>
+							<div className="flex items-center space-x-4">
+								<Avatar className="h-8 w-8">
+									<AvatarFallback>{fullName?.[0]}</AvatarFallback>
+								</Avatar>
+								<div>
+									<p className="text-sm font-medium leading-none">{fullName}</p>
+									<p className="text-sm text-muted-foreground">{lastMessage}</p>
+								</div>
 							</div>
+							<Popover>
+								<PopoverTrigger asChild>
+									<Button variant="outline" size="sm" className="ml-auto">
+										<ChevronDownIcon className="h-4 w-4 text-muted-foreground" />
+									</Button>
+								</PopoverTrigger>
+								<PopoverContent className="p-0 w-32" align="end">
+									<Command>
+										<CommandList>
+											<CommandGroup>
+												{options.map((option) => {
+													return (
+														<CommandItem
+															key={option.value}
+															className="flex flex-col items-start px-4 py-2"
+														>
+															<p>{option.label}</p>
+														</CommandItem>
+													);
+												})}
+											</CommandGroup>
+										</CommandList>
+									</Command>
+								</PopoverContent>
+							</Popover>
 						</div>
-						<Popover>
-							<PopoverTrigger asChild>
-								<Button variant="outline" size="sm" className="ml-auto">
-									<ChevronDownIcon className="h-4 w-4 text-muted-foreground" />
-								</Button>
-							</PopoverTrigger>
-							<PopoverContent className="p-0 w-32" align="end">
-								<Command>
-									<CommandList>
-										<CommandGroup>
-											{options.map((option) => (
-												<CommandItem
-													key={option.value}
-													className="flex flex-col items-start px-4 py-2"
-												>
-													<p>{option.label}</p>
-												</CommandItem>
-											))}
-										</CommandGroup>
-									</CommandList>
-								</Command>
-							</PopoverContent>
-						</Popover>
-					</div>
-				))}
+					);
+				})}
 			</CardContent>
 		</Card>
 	);
