56638c8b51b59c6e0ae9a7a2130d5039573099c7
Shayan Ameen <shayan.ameen.developer@gmail.com>
2024-08-29 08:47:35 +0500

    refactor: Simplify useRecentChats hook implementation

--- src/app/(chat)/recent-chats/page.tsx
+++ src/app/(chat)/recent-chats/page.tsx
@@ -1,15 +1,10 @@
 "use client";
 
-import { useEffect } from "react";
 import { useRecentChats } from "~/hooks/use-recent-chats";
 import { RecentsChats } from "./_components/recents-chats";
 
 export default function RecentChatsPage() {
 	const { onlineUsers, privateChats, groupChats } = useRecentChats();
-
-	useEffect(() => {
-		console.log({ onlineUsers }, { privateChats }, { groupChats });
-	}, [onlineUsers, privateChats, groupChats]);
 
 	return (
 		<section className="py-6 px-4">
