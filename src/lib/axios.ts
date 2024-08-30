ae290ed746c326548d2bcd23a207c3675025f99e
Shayan Ameen <shayan.ameen.developer@gmail.com>
2024-08-30 19:08:50 +0500

    refactor: Update axios and socket code for better authentication handling
    The axios and socket code has been refactored to improve authentication handling. This includes updating the authorization headers in axios requests to use the token directly, without the "Bearer" prefix, and using the "withCredentials" option in the socket configuration to ensure proper authentication. These changes enhance the security and reliability of the application's communication with the server.

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,33 +3,37 @@ import { cookies } from "next/headers";
 
 axios.interceptors.request.use((config) => {
 	const token = cookies().get("token")?.value;
+
 	if (token) {
-		config.headers.authorization = `Bearer ${token}`;
+		config.headers.authorization = token;
 	}
+
 	return config;
 });
 
 axios.interceptors.response.use(
 	(response) => {
 		if (response.data?.data?.token) {
-			cookies().set("token", response.data.data.token, {
+			cookies().set("token", `Bearer ${response.data.data.token}`, {
 				httpOnly: true,
 				secure: process.env.NODE_ENV === "production",
 				sameSite: "strict",
 				maxAge: 30 * 24 * 60 * 60,
 			});
 		}
+
 		return response;
 	},
 	(error) => {
 		if (error.response?.data?.data?.token) {
-			cookies().set("token", error.response.data.data.token, {
+			cookies().set("token", `Bearer ${error.response.data.data.token}`, {
 				httpOnly: true,
 				secure: process.env.NODE_ENV === "production",
 				sameSite: "strict",
 				maxAge: 30 * 24 * 60 * 60,
 			});
 		}
+
 		return Promise.reject(error);
 	},
 );
--- src/stores/socket.ts
+++ src/stores/socket.ts
@@ -19,9 +19,10 @@ onMount(socketStore, () => {
 	}
 
 	const socket = io("http://192.168.100.85:8080", {
-		extraHeaders: {
-			token: `Bearer ${"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwNjRiYmM4LWNhOGItNDMzNS1hZmM1LWVmYjFkNzRkMzQyZiIsImVtYWlsIjoiY2lhbzNAeW9wbWFpbC5jb20iLCJ0b2tlblR5cGUiOiJ2ZXJpZmljYXRpb24iLCJkZXZpY2VUb2tlbiI6InF3ZXJ0eXVpb3AiLCJkZXZpY2VUeXBlIjoid2ViIiwiaWF0IjoxNzI0NzYwMjc0fQ.mmgHwPWUsjaWcTG7E3OEwqOBIVKK63AXta96EyJZIgo"}`,
-		},
+		withCredentials: true,
+		// extraHeaders: {
+		// 	token: `Bearer ${"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwNjRiYmM4LWNhOGItNDMzNS1hZmM1LWVmYjFkNzRkMzQyZiIsImVtYWlsIjoiY2lhbzNAeW9wbWFpbC5jb20iLCJ0b2tlblR5cGUiOiJ2ZXJpZmljYXRpb24iLCJkZXZpY2VUb2tlbiI6InF3ZXJ0eXVpb3AiLCJkZXZpY2VUeXBlIjoid2ViIiwiaWF0IjoxNzI0NzYwMjc0fQ.mmgHwPWUsjaWcTG7E3OEwqOBIVKK63AXta96EyJZIgo"}`,
+		// },
 	});
 
 	socketStore.setKey("instance", socket);
@@ -48,7 +49,7 @@ onMount(socketStore, () => {
 		toast({
 			variant: "error",
 			title: "Socket Error",
-			description: error,
+			description: error?.message || "Connection Error",
 		});
 	});
 
@@ -60,7 +61,7 @@ onMount(socketStore, () => {
 		toast({
 			variant: "error",
 			title: "Socket Error",
-			description: error,
+			description: error?.message || "Unknown Error",
 		});
 	});
 
